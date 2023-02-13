06d5ae690c0ef64e1dfce4eb440565c0ec18134e
yegor bryzgalov <jackthehunterr@gmail.com>
2023-02-13 18:46:32 +0400

    Added ability to save account by login

--- README.md
+++ README.md
@@ -9,11 +9,10 @@ Create -> Choose template Python -> Create Repl.
 
 Drop all of repository files inside repl that you created, make sure that main file look's like this one.
 ![img.png](img.png)
-If you want to switch name of new accounts or started number than change 4 or 5 string.
 
 # IMPORTANT!!!
 
-You can change a template of your .json file, but if you won't do that BE SURE THAT YOU CHANGED 10'TH STRING (SUPER IMPORTANT!!!).
+You can change a template of your .json file(but SteamLogin and SteamPassword field values shold look like it's in example.json right now), but if you won't do that BE SURE THAT YOU CHANGED 10'TH STRING (SUPER IMPORTANT!!!).
 
 Run file, wait a couple of seconds and receive new files inside 'json_accounts' folder.
 
--- main.py
+++ main.py
@@ -1,12 +1,20 @@
 import json
 import os
 
-CONST_NEW_ACCOUNT_NAME = 'rexto_account-'
-CONST_NEW_ACCOUNT_STARTING_NUMBER = 1
+
 CONST_NEW_FOLDER_NAME = 'json_accounts'
-# FINAL RESULT WILL LOOK LIKE 'rexto_account-1.json' inside 'json_accounts' folder
+
 
 if __name__=='__main__':
+
+    CONST_ACCOUNT_JSON_TYPE_NAME = input \
+        ('Do you want to save account by login or every account will have the same name but different number?(Press 1 or 2)\n')
+    if (CONST_ACCOUNT_JSON_TYPE_NAME == '2'):
+        CONST_NEW_ACCOUNT_NAME = input("Print new account's name for example 'rexto_account-'\n")
+        CONST_NEW_ACCOUNT_STARTING_NUMBER = int(input("Print new account's starting number\n"))
+
+
+
     file1 = open('accs.txt', 'r')
     Lines = file1.readlines()
 
@@ -28,7 +36,14 @@ if __name__=='__main__':
         new_account_data = json.dumps(data)
         print(new_account_data)
 
-        with open(os.path.join(CONST_NEW_FOLDER_NAME, f"{CONST_NEW_ACCOUNT_NAME}{CONST_NEW_ACCOUNT_STARTING_NUMBER + count - 1}.json"), "w") as file:
-            file.write(new_account_data)
+        if(CONST_ACCOUNT_JSON_TYPE_NAME == '1'):
+            with open(os.path.join(CONST_NEW_FOLDER_NAME, f"{parse_array[0]}.json"), "w") as file:
+                file.write(new_account_data)
+        elif(CONST_ACCOUNT_JSON_TYPE_NAME == '2'):
+            with open(os.path.join(CONST_NEW_FOLDER_NAME,
+                                   f"{CONST_NEW_ACCOUNT_NAME}{CONST_NEW_ACCOUNT_STARTING_NUMBER + count - 1}.json"),
+                      "w") as file:
+                file.write(new_account_data)
+        else:(print('ERROR! Restart and choose "1" or "2" option'))
 
     new_account_json_file.close()
